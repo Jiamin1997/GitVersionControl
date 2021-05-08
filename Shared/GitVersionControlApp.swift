cfc6ae9f8781f3e599fa618ceef59b70a85b2edb
jiamin <lujiamin874916079@icloud.com>
2021-05-08 09:56:39 +0800

    Initial Commit

---
+++ Shared/GitVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControlApp.swift
+//  Shared
+//
+//  Created by 陆佳敏 on 2021/5/8.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
